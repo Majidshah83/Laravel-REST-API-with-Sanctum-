6a0bab850bf9df6916f8e7bedfb0e9bec6644918
Majidshah83 <shahmajid508@gmail.com>
2022-10-13 13:28:02 +0500

    register user and also genreate acess token

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,7 +4,24 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use App\Http\Requests\RegisterRequest;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Response;
 class AuthController extends Controller
 {
-    public function register(Request)
+    public function register(RegisterRequest $request)
+    {
+
+        $user=new User();
+        $user->name=$request->name;
+        $user->email=$request->email;
+        $user->password=Hash::make($request->password);
+        $user->save();
+        $token=$user->createToken('mytoken')->plainTextToken;
+        $response=[
+            'user'=>$user,
+            'token'=>$token
+        ];
+        return $response;
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Models\Product;
@@ -31,3 +32,4 @@ Route::group(['middleware' => ['auth:sanctum']], function() {
 Route::put('/updateProduct/{id}',[ProductController::class,'update']);
 Route::delete('/deleteProduct/{id}',[ProductController::class,'destroy']);
 });
+Route::post('/userRigster',[AuthController::class,'register']);
